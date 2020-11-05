ee6d0d905ab91a9de39942fca133c621fb2c401f
Mohammed Fathy <mohamedfathy573@gmail.com>
2020-11-05 15:52:22 +0200

    set database connection

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-3-218-75-21.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd2lbrf1qibskgm'),
+            'username' => env('DB_USERNAME', 'avuditzeqdblfs'),
+            'password' => env('DB_PASSWORD', 'd617d3fa7f459f0d1e229f4885817d804e152d3ac727b9903cefb724669f49ca'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
